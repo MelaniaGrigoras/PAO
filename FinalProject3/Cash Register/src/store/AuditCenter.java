5a6a715a3a01fc70de3bd835f72f687b2fa9bfbd
Melania <48444424+MelaniaGrigoras@users.noreply.github.com>
2020-06-11 00:23:50 +0300

    Update AuditCenter.java

--- FinalProject3/Cash Register/src/store/AuditCenter.java
+++ FinalProject3/Cash Register/src/store/AuditCenter.java
@@ -6,18 +6,45 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
 
-public class AuditCenter {
-	
-	public static void audit(String action) throws IOException {
-		Date date = new Date();
-		long time = date.getTime();
-		Timestamp ts = new Timestamp(time);
-		
+public class AuditCenter extends Thread {
 
-		FileWriter fileWriter = new FileWriter("C:\\Users\\domo\\Desktop\\AuditFile.csv", true);
-	    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-	    
-	    bufferedWriter.append(action + "," + ts + "\n");
-	    bufferedWriter.close();
-	}
+    private Thread t;
+    private String threadName;
+
+    public AuditCenter(String ...t) {
+        this.threadName = t[0];
+        System.out.println("Creating " + threadName);
+    }
+
+    public void run() {
+        System.out.println("Running " + threadName);
+        try{
+//            for(int i = 0; i < 5; i++) {
+                System.out.println("Thread " + threadName);
+                Thread.sleep(10000);
+//            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupted.");
+        }
+        System.out.println("Thread " + threadName + " exiting");
+
+    }
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+        if(t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+
+    public static void audit(String action, AuditCenter ...t) throws IOException {
+        Date date = new Date();
+        long time = date.getTime();
+        Timestamp ts = new Timestamp(time);
+        FileWriter fileWriter = new FileWriter("C:\\Users\\m_mel\\Desktop\\AuditFile.csv", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.append(t[0].threadName + "," + ts + "\n");
+        bufferedWriter.close();
+    }
 }
